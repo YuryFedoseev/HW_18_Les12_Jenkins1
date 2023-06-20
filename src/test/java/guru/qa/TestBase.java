94e432a42e3ec39af283bfde2a763d5bf0c57261
Yury.Fedoseev <Yri95@inbox.ru>
2023-06-20 19:59:09 +0300

    refactor selenoid 2

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -1,7 +1,11 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -22,5 +26,10 @@ public class TestBase  {
                 "enableVideo", true
                 ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
