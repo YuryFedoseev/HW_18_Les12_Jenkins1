95d21c627c50e9f4f0f27ee95484b464f5c1d97b
Yury.Fedoseev <Yri95@inbox.ru>
2023-06-20 19:55:13 +0300

    refactor selenoid

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -2,6 +2,9 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase  {
 
@@ -10,5 +13,14 @@ public class TestBase  {
         Configuration.browserSize = "720x880";
         Configuration.baseUrl = "https://github.com/YuryFedoseev/HW_18_Les11_Allure";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:option", Map.<String,Object>of(
+                "enableVNC",true,
+                "enableVideo", true
+                ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
