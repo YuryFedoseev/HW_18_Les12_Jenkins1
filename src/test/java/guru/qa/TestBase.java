25df5f904e405d04f03bb30088f93123affbe829
Yury.Fedoseev <Yri95@inbox.ru>
2023-06-20 19:46:48 +0300

    refactor

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -9,6 +9,6 @@ public class TestBase  {
     static void beforeAll() {
         Configuration.browserSize = "720x880";
         Configuration.baseUrl = "https://github.com/YuryFedoseev/HW_18_Les11_Allure";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
