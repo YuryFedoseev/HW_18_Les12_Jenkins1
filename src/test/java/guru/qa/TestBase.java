4bcb3442497a9f8f75a265afa4230c67d04a9306
Yury.Fedoseev <Yri95@inbox.ru>
2023-06-22 20:55:42 +0300

    includeTags

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -2,8 +2,8 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import net.bytebuddy.build.Plugin;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,21 +12,19 @@ import java.util.Map;
 public class TestBase {
 
 
-    @Tag("browser")
+
     @BeforeAll
     static void beforeAll() {
 
-        System.setProperty("browser","firefox");
-        String browserName = System.getProperty("browser");
-        Configuration.browser = browserName;
+        Configuration.browser = System.getProperty("browser", "chrome");
 
-        System.setProperty("browserVersion","98.0");
-        String browserVersion = System.getProperty("browserVersion");
-        Configuration.browserVersion = browserVersion;
+        Configuration.browserVersion = System.getProperty("browserVersion", "100" );
 
-        Configuration.browserSize = "720x880";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize","720x880" );
+
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
+
+        Configuration.remote = System.getProperty("remoteUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:option", Map.<String, Object>of(
